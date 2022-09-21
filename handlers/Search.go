83eb0fcb42d04603074de40b989fec1102fbc87d
arahiko-ayami <zinang7@gmail.com>
2022-09-21 22:58:20 +0700

    chore: add log on error

--- handlers/Search.go
+++ handlers/Search.go
@@ -1,6 +1,7 @@
 package handlers
 
 import (
+	"fmt"
 	"github.com/gofiber/fiber/v2"
 	"github.com/meilisearch/meilisearch-go"
 	"kma_score_api/utils"
@@ -17,6 +18,7 @@ func Search(c *fiber.Ctx) error {
 	search, err := client.Index("students").Search(query, &meilisearch.SearchRequest{})
 
 	if err != nil {
+		fmt.Println(err)
 		return c.Status(500).JSON(utils.ApiResponse(500, "Internal Server Error", nil))
 	}
 
