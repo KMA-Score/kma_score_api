39959203340b298c70815258994af43ee55f4bd9
arahiko-ayami <zinang7@gmail.com>
2022-10-01 18:57:46 +0700

    update: optimize json output

--- models/Score.go
+++ models/Score.go
@@ -1,9 +1,9 @@
 package models
 
 type Score struct {
-	ID                   int     `gorm:"column:Id" json:"id"`
-	StudentId            string  `gorm:"column:StudentId" json:"studentId"`
-	SubjectId            string  `gorm:"column:SubjectId" json:"subjectId"`
+	ID                   int     `gorm:"column:Id" json:"-"`
+	StudentId            string  `gorm:"column:StudentId" json:"-"`
+	SubjectId            string  `gorm:"column:SubjectId" json:"-"`
 	FirstComponentScore  string  `gorm:"column:FirstComponentScore" json:"firstComponentScore"`
 	SecondComponentScore string  `gorm:"column:SecondComponentScore" json:"secondComponentScore"`
 	ExamScore            string  `gorm:"column:ExamScore" json:"examScore"`
--- models/Subject.go
+++ models/Subject.go
@@ -4,7 +4,7 @@ type Subject struct {
 	ID              string  `gorm:"column:Id" json:"id"`
 	Name            string  `gorm:"column:Name" json:"name"`
 	NumberOfCredits int     `gorm:"column:NumberOfCredits" json:"numberOfCredits"`
-	Scores          []Score `gorm:"foreignKey:SubjectId" json:"scores"`
+	Scores          []Score `gorm:"foreignKey:SubjectId" json:"-"`
 }
 
 func (Subject) TableName() string {
