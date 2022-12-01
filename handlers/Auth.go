5df16eb749b4228986147095db960166a6f9ce75
Dang Hoang Phuc <dhp371.work@gmail.com>
2022-12-02 04:04:27 +0700

    feat: done auth token

--- handlers/Auth.go
+++ handlers/Auth.go
@@ -4,11 +4,24 @@ import (
 	"fmt"
 	"github.com/gofiber/fiber/v2"
 	"github.com/golang-jwt/jwt"
+	"github.com/mitchellh/mapstructure"
+	"kma_score_api/utils"
 	"log"
+	"os"
+	"time"
 )
 
 type AuthTokenType struct {
 	Token string `json:"token"`
+}
+
+type TokenBody struct {
+	Email string
+	Exp   int64
+	Iat   int64
+	Id    string
+	Name  string
+	MsExp int64
 }
 
 func AuthToken(c *fiber.Ctx) error {
@@ -16,20 +29,60 @@ func AuthToken(c *fiber.Ctx) error {
 
 	if err := c.BodyParser(p); err != nil {
 		log.Println(err)
+		return c.Status(400).JSON(utils.ApiResponse(400, "Body parse failed", ""))
 	}
 
-	token, err := jwt.Parse(p.Token, func(token *jwt.Token) (interface{}, error) {
-		// Don't forget to validate the alg is what you expect:
-		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
-			return nil, fmt.Errorf("Unexpected signing method: %v", token.Header["alg"])
-		}
+	if len(p.Token) == 0 {
+		return c.Status(400).JSON(utils.ApiResponse(400, "Missing Token param", nil))
+	}
 
-		// hmacSampleSecret is a []byte containing your secret, e.g. []byte("my_secret_key")
-		return hmacSampleSecret, nil
-	})
+	isValid, err := utils.JwtVerifyToken(p.Token, os.Getenv("JWT_CLIENT_PUBLIC_KEY"))
+
 	if err != nil {
-		panic(err)
+		log.Println(err)
+		return c.Status(500).JSON(utils.ApiResponse(500, "Error in validate token", nil))
 	}
-	claims := token.Claims.(jwt.MapClaims)
 
+	if isValid == false {
+		return c.Status(400).JSON(utils.ApiResponse(400, "Token is invalid", nil))
+	}
+
+	// TODO: Figure out how to Parse and Validate in one step. Make it faster
+	//rsaPrivate, err := jwt.ParseRSAPrivateKeyFromPEM([]byte(os.Getenv("JWT_CLIENT_PUBLIC_KEY")))
+
+	claims := jwt.MapClaims{}
+
+	_, _, err = new(jwt.Parser).ParseUnverified(p.Token, claims)
+
+	if err != nil {
+		fmt.Println(err)
+		return c.Status(500).JSON(utils.ApiResponse(500, "Parse token failed", nil))
+	}
+
+	// Dirty hack convert map to struct
+	var tokenStruct TokenBody
+	err = mapstructure.Decode(claims, &tokenStruct)
+
+	if err != nil {
+		fmt.Println(err)
+		return c.Status(500).JSON(utils.ApiResponse(500, "Parse token struct failed", nil))
+	}
+
+	// Check token valid exp
+	if tokenStruct.MsExp < time.Now().UTC().Unix() {
+		return c.Status(403).JSON(utils.ApiResponse(403, "Token expired", nil))
+	}
+
+	newToken, err := utils.JWTConstructToken(claims, os.Getenv("JWT_SERVER_PRIVATE_KEY"))
+
+	if err != nil {
+		fmt.Println(err)
+		return c.Status(500).JSON(utils.ApiResponse(500, "Generate token failed", nil))
+	}
+
+	rsp := fiber.Map{
+		"token": newToken,
+	}
+
+	return c.Status(200).JSON(utils.ApiResponse(200, "Success", rsp))
 }
